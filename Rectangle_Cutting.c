42d81704dfb2d8a82799f3f492715836a2a75d3b
sakib-mustafa-ak <sakib.mustafa.ak@gmail.com>
2024-02-11 22:40:15 +0600

    fisrt

--- Rectangle_Cutting.c
+++ Rectangle_Cutting.c
@@ -4,19 +4,15 @@ int main(){
     scanf("%ld",&t);
     while(t--){
         scanf("%ld%ld",&a,&b);
-        if(a>1&&b>1){
-            if(a%2 != 0 && b%2 != 0){
+            if(a%2 == 1 && b%2 == 1 ){
                 printf("No\n");
-            } else if (b%2 != 0 && a/2 == b){ 
+            } else if (a%2 == 0 && b%2 == 1 && a/2 == b){ 
                 printf("No\n");
-            }else if (a%2 != 0 && b/2 == a){ 
+            } else if (b%2 == 0 && a%2 == 1 && b/2 == a){ 
                 printf("No\n");
-            }else {
+            } else {
                 printf("Yes\n");
             }
-        } else {
-            printf("No\n");
-        }
     }
     return 0;
 }
