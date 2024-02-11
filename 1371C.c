a35dbf4f45b57470853b70dc0617533e881921f2
sakib-mustafa-ak <sakib.mustafa.ak@gmail.com>
2024-02-12 01:03:09 +0600

    first

--- 1371C.c
+++ 1371C.c
@@ -1,5 +1,18 @@
 #include<stdio.h>
 int main(){
-    
+    long long v , c , n , m , t ,x;
+    scanf("%lld",&t);
+    while (t--){
+        scanf("%lld%lld%lld%lld",&v,&c,&n,&m);
+        if (v>c&&c>=m&&(c-m+v)>=n){
+            printf("Yes\n");
+        } else if (v<c&&v>=m&&(v-m+c)>=n){
+            printf("Yes\n");
+        } else if (v==c&&v>=m&&(v-m+c)>=n){
+            printf("Yes\n");
+        } else {
+            printf("No\n");
+        }      
+    }
     return 0;
 }
