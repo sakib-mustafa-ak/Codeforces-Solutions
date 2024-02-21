984a90cd2525d9e669ae030405fdf2404596ea74
sakib-mustafa-ak <sakib.mustafa.ak@gmail.com>
2024-02-21 18:43:54 +0600

    first

--- 110A.c
+++ 110A.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
-#include <string.h>
+
 int main()
 {
+    char a[20];
+    gets(a);
+    long long count = 0;
+    for (long long i = 0; i < 20; i++)
+    {
+        if (a[i] == '4' || a[i] == '7')
+        {
+            count++;
+        }
+    }
+    if (count == 4 || count == 7)
+    {
+        printf("YES\n");
+    }
+    else
+    {
+        printf("NO\n");
+    }
 }
