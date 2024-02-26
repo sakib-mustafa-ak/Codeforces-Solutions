264463151f3cd0d0c4e1669dc30641edf0308a18
sakib-mustafa-ak <sakib.mustafa.ak@gmail.com>
2024-02-26 18:02:17 +0600

    first

---
+++ 337A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    int a[m];
+    for (int i = 0; i < m; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a, a + m);
+    int d = 0, min = a[n - 1] - a[0];
+    for (int i = 1; i <= m - n; i++)
+    {
+        d = a[i + n - 1] - a[i];
+        if (d < min)
+        {
+            min = d;
+        }
+    }
+    cout << min << endl;
+}
