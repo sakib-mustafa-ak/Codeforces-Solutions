5ebfadad04edfa98b89de25679e1abef869eab17
sakib-mustafa-ak <sakib.mustafa.ak@gmail.com>
2024-03-01 16:05:19 +0600

    first

--- 313A.cpp
+++ 313A.cpp
@@ -5,11 +5,15 @@ int main()
 {
     long long n;
     cin >> n;
+    int num;
     if (n >= 0)
     {
         cout << n << endl;
     }
     else
     {
+        int x = n / 10;
+        int y = (n / 100) * 10 + n % 10;
+        cout << max(x, y) << endl;
     }
 }
