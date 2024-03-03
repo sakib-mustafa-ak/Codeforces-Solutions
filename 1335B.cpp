23082587db26779db84d3e6df6803a09658dea64
sakib-mustafa-ak <sakib.mustafa.ak@gmail.com>
2024-03-03 21:18:43 +0600

    first

---
+++ 1335B.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+char alphabates[] = "abcdefghijklmnopqrstuvwxyz";
+void solve()
+{
+    int a, n, b;
+    cin >> n >> a >> b;
+    string s[n];
+    for (int i = 0; i < b; i++)
+    {
+        s[i] = alphabates[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << s[i % b];
+    }
+    cout << endl;
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
