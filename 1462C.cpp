ee4270dfbf97e62aa8f25c195b5e3a3ea50fc844
sakib-mustafa-ak <sakib.mustafa.ak@gmail.com>
2024-03-10 01:48:34 +0600

    first

--- 1462C.cpp
+++ 1462C.cpp
@@ -4,7 +4,43 @@ void solve()
 {
     int x;
     cin >> x;
+    vector<int> ans;
+    for (int i = 9; i > 0; i--)
+    {
+
+        x -= i;
+        if (x <= 0)
+        {
+            ans.push_back(x + i);
+
+            break;
+        }
+        else
+        {
+            ans.push_back(i);
+        }
+    }
+
+    if (x <= 0)
+    {
+        sort(ans.begin(), ans.end());
+        for (int i = 0; i < ans.size(); i++)
+        {
+            cout << ans[i];
+        }
+    }
+    else
+    {
+        cout << "-1";
+    }
+    cout << endl;
 }
 int main()
 {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
 }
