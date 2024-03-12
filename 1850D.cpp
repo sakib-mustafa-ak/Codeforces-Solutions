a307b698f82a8373c82a503a1da916be81bb0bdd
sakib-mustafa-ak <sakib.mustafa.ak@gmail.com>
2024-03-12 22:35:28 +0600

    first

--- 1850D.cpp
+++ 1850D.cpp
@@ -10,8 +10,8 @@ void solve()
         cin >> a[i];
     }
     sort(a, a + n);
-    long long count = 0, max = 0;
-    for (long long i = 0; i < n; i++)
+    long long count = 0, ass = 0;
+    for (long long i = 0; i < n - 1; i++)
     {
         if (a[i + 1] - a[i] <= k)
         {
@@ -19,14 +19,15 @@ void solve()
         }
         else
         {
-            if (count > max)
+            if (count > ass)
             {
-                max = count;
+                ass = count;
             }
             count = 0;
         }
     }
-    cout << n - max - 1 << endl;
+    ass = max(count, ass);
+    cout << n - ass - 1 << endl;
 }
 int main()
 {
