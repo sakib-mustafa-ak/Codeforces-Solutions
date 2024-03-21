e0b821d603750b1e1fdad7c9f9374360aa52843a
sakib-mustafa-ak <sakib.mustafa.ak@gmail.com>
2024-03-22 02:03:01 +0600

    0

--- 1742C.cpp
+++ 1742C.cpp
@@ -1,104 +1,81 @@
 #include <bits/stdc++.h>
 using namespace std;
+void solve()
+{
+    char a[8][8];
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+    int count = 1, count2 = 0, end = 0;
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 1; j < 8; j++)
+        {
+            if (a[i][0] == '.')
+            {
+                break;
+            }
+            if (a[i][0] == a[i][j])
+            {
+                count++;
+            }
+            else
+            {
+                count = 1;
+                break;
+            }
+        }
+        if (count == 8)
+        {
+            cout << a[i][0] << endl;
+            count = 1;
+            break;
+        }
+        else
+        {
+            count = 1;
+        }
+    }
+    for (int j = 0; j < 8; j++)
+    {
+        for (int i = 1; i < 8; i++)
+        {
+            if (a[0][j] == '.')
+            {
+                break;
+            }
+            if (a[0][j] == a[i][j])
+            {
+                count++;
+            }
+            else
+            {
+                count = 1;
+                break;
+            }
+        }
+        if (count == 8)
+        {
+            cout << a[0][j] << endl;
+            count = 1;
+            break;
+        }
+        else
+        {
+            count = 1;
+        }
+    }
+}
 int main()
 {
     int t;
     cin >> t;
     while (t--)
     {
-        char a[8][8];
-        for (int i = 0; i < 8; i++)
-        {
-            for (int j = 0; j < 8; j++)
-            {
-                cin >> a[i][j];
-            }
-        }
-        int r = 0, b = 0, count = 0;
-        for (int i = 0; i < 8; i++)
-        {
-            for (int j = 0; j < 8; j++)
-            {
-                if (a[i][j] == 'R')
-                {
-                    r++;
-                }
-                else if (a[i][j] == 'B')
-                {
-                    b++;
-                }
-            }
-            if (r == 8)
-            {
-                cout << "R" << endl;
-                break;
-            }
-            else if (b == 8)
-            {
-                cout << "B" << endl;
-                break;
-            }
-            else
-            {
-                if (r < b)
-                {
-                    for (int j = 0; j < 8; j++)
-                    {
-                        if (a[i][j] == 'R')
-                        {
-                            for (int i = 0; i < 8; i++)
-                            {
-                                if (a[i][j] == 'R')
-                                {
-                                    count++;
-                                }
-                            }
-                            if (count == 8)
-                            {
-                                break;
-                            }
-                            else
-                            {
-                                count = 0;
-                            }
-                        }
-                    }
-                    if (count == 8)
-                    {
-                        cout << "R" << endl;
-                        break;
-                    }
-                }
-                else if (b < r)
-                {
-                    for (int j = 0; j < 8; j++)
-                    {
-                        if (a[i][j] == 'B')
-                        {
-                            for (int i = 0; i < 8; i++)
-                            {
-                                if (a[i][j] == 'B')
-                                {
-                                    count++;
-                                }
-                            }
-                            if (count == 8)
-                            {
-                                break;
-                            }
-                            else
-                            {
-                                count = 0;
-                            }
-                        }
-                    }
-                    if (count == 8)
-                    {
-                        cout << "R" << endl;
-                        break;
-                    }
-                }
-            }
-        }
+        solve();
     }
 }
