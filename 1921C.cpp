2c30dc202f9ab748020c7a89ae1d3f49901af17d
sakib-mustafa-ak <sakib.mustafa.ak@gmail.com>
2024-04-11 21:48:36 +0600

    first

--- 1921C.cpp
+++ 1921C.cpp
@@ -3,31 +3,24 @@ using namespace std;
 #define ll long long
 void solve()
 {
-    ll n, f, a, b;
+    ll n, f, a, b, token = 0;
+
     cin >> n >> f >> a >> b;
-    vector<ll> arr;
-    for (ll i = 0; i < n; i++)
+
+    ll arr[n + 1];
+    arr[0] = 0;
+    for (ll i = 1; i <= n; i++)
     {
-        ll x;
-        cin >> x;
-        arr.push_back(x);
+        cin >> arr[i];
     }
-    for (ll j = 0; j < n; j++)
+
+    for (ll j = 1; j <= n; j++)
     {
-        for (ll i = 0; i <= arr[n - 1]; i++)
-        {
-            if ((arr[j] - i) * a > b)
-            {
-                f -= b;
-                i = arr[j];
-            }
-            else
-                f -= a;
-            if (f <= 0)
-                break;
-        }
+        f -= min((arr[j] - arr[j - 1]) * a, b);
+        if (f <= 0)
+            break;
     }
-    (f <= 0) ? cout << "No\n" : cout << "Yes\n";
+    (f > 0) ? cout << "Yes\n" : cout << "No\n";
 }
 
 int main()
