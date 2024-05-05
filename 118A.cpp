64ab57a743b3d1819b566003eb268e6b7cfaf99f
sakib-mustafa-ak <sakib.mustafa.ak@gmail.com>
2024-05-05 16:00:18 +0600

    first

---
+++ 118A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'y' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U' || s[i] == 'Y')
+        {
+        }
+        else
+        {
+            if (s[i] < 97)
+            {
+                char x = s[i] + 32;
+                cout << "." << x;
+            }
+            else
+                cout << "." << s[i];
+        }
+    }
+    cout << endl;
+}
