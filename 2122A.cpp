c10c2213974bb7f68b61f2baa261baad092bae9c
Sakib Mustafa <sakib.mustafa.co@gmail.com>
2025-07-20 21:16:04 +0600

    1

--- 2122A.cpp
+++ 2122A.cpp
@@ -1,1 +1,23 @@
-2122A.cpp
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define Start ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define ff first
+#define ss second
+void solve(){
+    int n,m; cin>>n>>m;
+    if(n==1 or m==1 or(n==2 and m==2)) cout<<"NO\n";
+    else cout<<"YES\n";
+}
+
+signed main(){
+    Start
+    int t = 1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
