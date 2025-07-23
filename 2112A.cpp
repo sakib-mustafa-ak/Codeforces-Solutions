8997140dc60dd3ce235131d46c951834e72d7c97
Sakib Mustafa <sakib.mustafa.co@gmail.com>
2025-07-23 21:59:41 +0600

    1

---
+++ 2112A.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define Start ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define ff first
+#define ss second
+void solve(){
+    int a,x,y; cin>>a>>x>>Y;
+
+    if((a<x and a<y) or (a>x and a>y)) cout<<"YES\n";
+    else cout<<"NO\n";
+}
+
+signed main(){
+    Start
+    int t = 1;
+    //cin>>t;
+    while(t--){
+        solve();
+    }
+}
